e0d386e396cdd5ed61a3b0af6550597a43e27638
Abhilasha Tandon <abhilashatandon167@gmail.com>
2025-01-17 00:54:06 -0500

    More experimentation with elo ranking

--- elo.py
+++ elo.py
@@ -5,7 +5,7 @@ from itertools import combinations
 class Tournament:
     # Elo tournament manager
     def __init__(self, entries):
-        self.entries = {entry: 1200 for entry in entries}
+        self.entries = {entry: 1200 + np.random.random() for entry in entries}
         items = list(self.entries.items())
         np.random.shuffle(items)
         self.entries = dict(items)
@@ -17,10 +17,11 @@ class Tournament:
         items = list(self.entries.items())
         # np.random.shuffle(items)
         self.entries = dict(items)
-        self.matches = np.random.shuffle(list(combinations(items, 2)))
+        self.matches = list(combinations(items, 2))
+        np.random.shuffle(self.matches)
         if len(self.matches[-1]) == 1:
             self.matches.pop()
-        self.k = self.k + 1
+        self.k += 1
         # gradually increase k as scores become more accurate
 
     def next_match(self):
@@ -39,9 +40,22 @@ class Tournament:
                 1.0 + 10.0 ** ((self.entries[entry1] - self.entries[entry2]) / 400.0)
             )
 
-        score_exchange = (10 / self.k + 16) * (result - expected_score)
+        score_exchange = (20 / self.k + 10) * (result - expected_score)
         self.entries[entry1] += score_exchange
         self.entries[entry2] -= score_exchange
+
+        # print(self.entries.values())
+
+
+def sortedness(l):
+    # assumes l is a permutation of range(n) for some n
+    n = len(l)
+    scores = []
+    for idx, value in enumerate(l):
+        delta_pos = idx - value
+        # how far off the item is from the correct place
+        scores.append(delta_pos)
+    return np.std(scores), max(scores)
 
 
 def is_sorted(l):
@@ -50,31 +64,32 @@ def is_sorted(l):
 
 def run_experiment(num_values, max_comp):
     tour = Tournament(list(range(num_values)))
+    sortedness_vals = []
     for i in range(max_comp):
         pair = tour.next_match()
-        if abs(pair[0][0] - pair[1][0]) < np.sqrt(num_values):
-            tour.update(pair[0][0], pair[1][0], 0.5)
-        elif pair[0][0] < pair[1][0]:
+        # if abs(pair[0][0] - pair[1][0]) < np.sqrt(num_values):
+        # tour.update(pair[0][0], pair[1][0], 0.5)
+        if pair[0][0] < pair[1][0]:
             # print(pair, 0)
             tour.update(pair[0][0], pair[1][0], 0)
         elif pair[0][0] > pair[1][0]:
             # print(pair, 1)
             tour.update(pair[0][0], pair[1][0], 1)
-        if is_sorted(list(tour.entries.keys())):
-            return i
+
         sorted_entries = sorted(tour.entries.items(), key=lambda item: item[1])
-        print([x[0] for x in sorted_entries])
-    return max_comp
+        sortedness_vals.append(sortedness([x[0] for x in sorted_entries]))
+    return np.array(sortedness_vals)
 
 
 def main():
-    for num_values in range(3, 40):
+    for num_values in range(3, 40, 2):
         num_tests = 100
-        sum_iter = 0
+        max_comp = 2 * num_values
+        sum_iter = np.zeros((max_comp, 2))
         for test in range(num_tests):
-            sum_iter += run_experiment(num_values, 10000)
-        print(num_values, sum_iter / num_tests)
+            sum_iter += run_experiment(num_values, max_comp)
+        print(num_values, sum_iter[0] / num_tests, sum_iter[-1] / num_tests, sep="\t")
 
 
 if __name__ == "__main__":
-    run_experiment(100, 100)
+    main()
